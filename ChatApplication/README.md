4444756c4bd7fecdb7a15f4ccd81cc79cca11943
Phước <vanphuoc3012@gmail.com>
2023-03-09 13:24:13 +0700

    chat app commit

---
+++ ChatApplication/README.md
@@ -0,0 +1,12 @@
+# Week 10-3, Practice
+
+## Section 1:
+
+### Task 1: Create Chat Application Using Websocket:
+
+## Section 2:
+### Task 1: Add Kafka to project
+
+### Task 2: Run project using Docker
+
+### Task 3: create
--- ChatApplication/src/main/java/com/example/chatapplication/configuration/kafka/KafkaController.java
+++ ChatApplication/src/main/java/com/example/chatapplication/configuration/kafka/KafkaController.java
@@ -1,16 +1,24 @@
 package com.example.chatapplication.configuration.kafka;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class KafkaController {
     @Autowired
     private KafkaListenerService kafkaListenerService;
+    @Autowired
+    private AdminClient adminClient;
 
     @Secured("ROLE_ADMIN")
     @GetMapping("/kafka")
@@ -21,5 +29,19 @@ public class KafkaController {
             throw new RuntimeException(e);
         }
         return ResponseEntity.ok("Change listener to topic: " + topic);
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/kafka/create-topic")
+    public ResponseEntity<?> createTopic(@RequestParam String topic) {
+        adminClient.createTopics(List.of(TopicBuilder.name(topic).build()));
+        return ResponseEntity.ok("Successfully create topic: " + topic);
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/kafka/delete-topic")
+    public ResponseEntity<?> deleteTopic(@RequestParam String topic) {
+        adminClient.deleteTopics(List.of(topic));
+        return ResponseEntity.ok("Successfully delete topic: " + topic);
     }
 }
